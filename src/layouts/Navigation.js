8ac35a60da19ed7aa2523e5a0ed87212dd558bd6
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-31 21:02:03 +0100

    create menu in Navigation component

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/Navigation.css';
+
+const menu = [
+    { name: 'home', path: '/' },
+    { name: 'about', path: 'about' },
+    { name: 'gallery', path: 'gallery' },
+    { name: 'contact', path: 'contact' },
+]
 
 const Navigation = () => {
+
+    const list = menu.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} className={item.className}>{item.name}</NavLink>
+        </li>
+    ))
+
     return (
-        <div>Navigation</div>
+
+        <nav>
+            <ul>
+                {list}
+            </ul>
+        </nav>
     );
 }
 
