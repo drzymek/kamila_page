fa49fbf2d3a1b898ce7be83aba7df03cc6eee7b0
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-31 23:54:09 +0100

    add files

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import About from '../pages/About';
+import Gallery from '../pages/Gallery';
+import Contact from '../pages/Contact';
+import Error from '../pages/Error';
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <>
+            <Switch>
+                <Route path="/about" exact component={About} />
+                <Route path="/gallery" component={Gallery} />
+                <Route path="/contact" component={Contact} />
+                <Route component={Error} />
+            </Switch>
+        </>
+
     );
 }
 
