4a3a0ceed884f9311bf00ca2e71796a586bea9f1
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-31 21:01:16 +0100

    add new logo image

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from './Navigation';
 import '../css/Header.css';
-import logo from '../img/newLogo.png';
+import logo from '../img/newLogo(1).png';
 
 
 const Header = () => {
