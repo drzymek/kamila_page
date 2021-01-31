60290108fb41c9378f1655a75c6bbf8c7ad2ed52
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-31 22:52:42 +0100

    update Hero component and styles

--- src/layouts/Hero.js
+++ src/layouts/Hero.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../css/Hero.css'
+
+import img1 from '../img/heroPhotos/img01.jpg';
+import img2 from '../img/heroPhotos/img02.jpg';
+import img3 from '../img/heroPhotos/img03.jpg';
+import img4 from '../img/heroPhotos/img04.jpg';
+import img5 from '../img/heroPhotos/img05.jpg';
+import img6 from '../img/heroPhotos/img06.jpg';
 
 const Hero = () => {
+
+    const heroPhotos = [img1, img2, img3, img4, img5, img6];
+    const photoIndex = Math.floor(Math.random() * heroPhotos.length);
+    const heroPhoto = () => {
+        return (heroPhotos[photoIndex])
+    }
     return (
-        <div>Hero</div>
+        <div className="hero">
+            <img src={heroPhoto()} alt="" />
+        </div>
     );
 }
 
