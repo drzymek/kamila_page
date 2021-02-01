292138f97d7b81a21db167819af77e370ffde67c
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-01 20:51:05 +0100

    add files

---
+++ src/layouts/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about scrollToPage">
+            <div>About</div>
+        </div>
+    );
+}
+
+export default About;
+<div>About</div>
