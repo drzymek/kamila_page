424cf871f06b959f69110638bb1d67d95db42280
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-01 20:52:45 +0100

    add handleScrollToPage function

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -3,17 +3,27 @@ import { NavLink } from 'react-router-dom';
 import '../css/Navigation.css';
 
 const menu = [
-    { name: 'home', path: '/' },
+    { name: 'home', path: '/', exact: true },
     { name: 'about', path: 'about' },
     { name: 'gallery', path: 'gallery' },
     { name: 'contact', path: 'contact' },
 ]
 
+const handleScrollToPage = () => {
+    const page = document.querySelector('.scrollToPage');
+    const pageFromTop = page.offsetTop;
+    window.scroll({
+        top: pageFromTop,
+        behavior: 'smooth'
+    })
+}
+
+
 const Navigation = () => {
 
     const list = menu.map(item => (
         <li key={item.name}>
-            <NavLink to={item.path} className={item.className}>{item.name}</NavLink>
+            <NavLink to={item.path} className={item.className} onClick={handleScrollToPage}>{item.name}</NavLink>
         </li>
     ))
 
