59a7573637e985fe14e183a89103cb9df7c5e248
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-01 23:11:14 +0100

    update About component + styles

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../css/About.css';
+import aboutPhoto from '../img/about/about_photo.jpg'
 
 const About = () => {
     return (
         <div className="about scrollToPage">
-            <div>About</div>
+            <aside className="aboutPhoto">
+                <img src={aboutPhoto} alt="" />
+            </aside>
+            <article>
+                <h2>Hello my name is <strong>Kamila</strong></h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti, ratione accusantium perspiciatis ipsum inventore quaerat facere dolorem exercitationem placeat molestias delectus dolores explicabo voluptatum facilis non consectetur officia repellat!</p>
+            </article>
         </div>
     );
 }
