0c1b255483a8f44b3739137f85a90dbbdab98a0b
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-01 23:29:29 +0100

    update content and styles in About component

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -4,15 +4,15 @@ import aboutPhoto from '../img/about/about_photo.jpg'
 
 const About = () => {
     return (
-        <div className="about scrollToPage">
+        <>
             <aside className="aboutPhoto">
                 <img src={aboutPhoto} alt="" />
             </aside>
             <article>
-                <h2>Hello my name is <strong>Kamila</strong></h2>
+                <h1>Hello my name is <strong>Kamila</strong></h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti, ratione accusantium perspiciatis ipsum inventore quaerat facere dolorem exercitationem placeat molestias delectus dolores explicabo voluptatum facilis non consectetur officia repellat!</p>
             </article>
-        </div>
+        </>
     );
 }
 
