3e225c949d4dc3c05ee66ff0fb9eaf1726223dc1
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-02 00:30:52 +0100

    update Gallery content, add mdb slider

--- src/layouts/Gallery.js
+++ src/layouts/Gallery.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React from "react";
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
+    "mdbreact";
+import img01 from '../img/heroPhotos/img01.jpg'
+import img02 from '../img/heroPhotos/img02.jpg'
+import img03 from '../img/heroPhotos/img03.jpg'
+import img04 from '../img/heroPhotos/img04.jpg'
+import img05 from '../img/heroPhotos/img05.jpg'
 
 const Gallery = () => {
     return (
-        <div className="gallery scrollToPage">Gallery</div>
+        <MDBContainer>
+            <MDBCarousel
+                activeItem={1}
+                length={5}
+                showControls={true}
+                showIndicators={false}
+                className="z-depth-1"
+                slide
+            >
+                <MDBCarouselInner>
+                    <MDBCarouselItem itemId="1">
+                        <MDBView>
+                            <img
+                                className="d-block w-100"
+                                src={img01}
+                                alt="First slide"
+                            />
+                        </MDBView>
+                    </MDBCarouselItem>
+                    <MDBCarouselItem itemId="2">
+                        <MDBView>
+                            <img
+                                className="d-block w-100"
+                                src={img02}
+                                alt="Second slide"
+                            />
+                        </MDBView>
+                    </MDBCarouselItem>
+                    <MDBCarouselItem itemId="3">
+                        <MDBView>
+                            <img
+                                className="d-block w-100"
+                                src={img03}
+                                alt="Third slide"
+                            />
+                        </MDBView>
+                    </MDBCarouselItem>
+                    <MDBCarouselItem itemId="4">
+                        <MDBView>
+                            <img
+                                className="d-block w-100"
+                                src={img04}
+                                alt="Fourth slide"
+                            />
+                        </MDBView>
+                    </MDBCarouselItem>
+                    <MDBCarouselItem itemId="5">
+                        <MDBView>
+                            <img
+                                className="d-block w-100"
+                                src={img05}
+                                alt="Fifth slide"
+                            />
+                        </MDBView>
+                    </MDBCarouselItem>
+                </MDBCarouselInner>
+            </MDBCarousel>
+        </MDBContainer>
     );
 }
 
