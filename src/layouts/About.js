a12cab4ff8934d75b48a8db13b1c8eadd901e574
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-02 22:36:44 +0100

    update About.js, new content and styles

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import '../css/About.css';
-import aboutPhoto from '../img/about/about_photo.jpg'
+import aboutPhoto from '../img/about/about_photo(1).jpg'
 
 const About = () => {
     return (
         <>
+            <span className="aboutSpan">a<br />b<br />o<br />u<br />t<br /><br /></span>
             <aside className="aboutPhoto">
                 <img src={aboutPhoto} alt="" />
             </aside>
             <article>
-                <h1>Hello my name is <strong>Kamila</strong></h1>
+                <h1>Hi, my name is <span className="nameAbout">Kamila</span></h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti, ratione accusantium perspiciatis ipsum inventore quaerat facere dolorem exercitationem placeat molestias delectus dolores explicabo voluptatum facilis non consectetur officia repellat!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam deleniti rerum pariatur nisi maxime incidunt voluptatibus facilis esse, consequatur nesciunt quia fugit accusantium optio odio hic consequuntur praesentium quaerat laudantium.</p>
+                <div className="social">
+                    <a href="##"><i class="fab fa-facebook-f"></i></a>
+                    <a href="##"><i class="fab fa-pinterest-square"></i></a>
+                    <a href="##"><i class="fab fa-instagram-square"></i></a></div>
             </article>
         </>
     );
