160d4170eaee1988c30b8971b18ba487f3543455
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-02 15:35:13 +0100

    add paragraph to About.js

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -11,6 +11,7 @@ const About = () => {
             <article>
                 <h1>Hello my name is <strong>Kamila</strong></h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid deleniti, ratione accusantium perspiciatis ipsum inventore quaerat facere dolorem exercitationem placeat molestias delectus dolores explicabo voluptatum facilis non consectetur officia repellat!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam deleniti rerum pariatur nisi maxime incidunt voluptatibus facilis esse, consequatur nesciunt quia fugit accusantium optio odio hic consequuntur praesentium quaerat laudantium.</p>
             </article>
         </>
     );
