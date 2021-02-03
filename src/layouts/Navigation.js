08ec2994598cb716616dde3e3c82d3caf76868e4
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-03 22:58:05 +0100

    add new logic to navigation

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { useScrollSections } from 'react-scroll-section';
 import '../css/Navigation.css';
 
+const menuList = [
+    { name: "home", className: "menu-item", id: "#home" },
+    { name: "about", className: "menu-item", id: "#about" },
+    { name: "gallery", className: "menu-item", id: "#gallery" },
+    { name: "contact", className: "menu-item", id: "#contact" }
+]
+
+const scrollToPage = () => {
+    const pageFromTop = document.querySelector('.navScrollToPage').offsetTop;
+    window.scroll({
+        top: pageFromTop
+    })
+}
+
 const Navigation = () => {
-    const sections = useScrollSections()
+    const menu = menuList.map(item => (
+        <li key={item.name}>
+            <a href={item.id} className={item.className} id={item.id} onClick={scrollToPage} >{item.name}</a>
+        </li>
+    ))
 
     return (
 
         <nav>
             <ul>
-                {sections.map(({ id, onClick, selected }) => (
-                    <li key={id} onClick={onClick} selected={selected}>
-                        {id.toUpperCase()}
-                    </li>
-                ))}
+                {menu}
             </ul>
         </nav>
     );
