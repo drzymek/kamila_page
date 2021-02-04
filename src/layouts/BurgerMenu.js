6d5266997f2315e4fb358db4b54df52071ac959b
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-04 22:15:16 +0100

    remove unused code

--- src/layouts/BurgerMenu.js
+++ src/layouts/BurgerMenu.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import { slide as Menu } from 'react-burger-menu';
 import '../css/BurgerMenu.css';
 
 const menuList = [
@@ -9,34 +8,19 @@ const menuList = [
     { name: "contact", className: "menu-item", id: "#contact" }
 ]
 
-// const scrollToPage = () => {
-//     const pageFromTop = document.querySelector('.navScrollToPage').offsetTop;
-//     window.scroll({
-//         top: pageFromTop,
-//         behavior: 'smooth'
-//     })
-// }
-
 const menu = menuList.map(item => (
     <li key={item.name}>
         <a href={item.id} className={item.className} id={item.id} >{item.name}</a>
     </li>
 ))
 
-// class BurgerMenu extends Component {
 export default function BurgerMenu() {
-    // showSettings(e)
-    // e.preventDefault();
 
-
-    /* show/hide menu, with hooks */
     const [isActive, setActive] = useState('false');
 
     const handleToggleNav = () => {
         setActive(!isActive);
     };
-
-    // render() {
     return (
         <>
             <div className={`arrow ${isActive ? '' : 'on'}`} onClick={handleToggleNav}>
@@ -49,5 +33,4 @@ export default function BurgerMenu() {
             </nav>
         </>
     );
-    // }
 }
