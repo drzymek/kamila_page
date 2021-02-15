7c3f287e3d6afbf71c1616bc10e9e1dd85ac6bab
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-02-15 15:12:08 +0100

    change index.scss name, delete unused css file, update import css files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import './css/index.css';
+import './css/styles.css';
 import App from './layouts/App';
 
 ReactDOM.render(
